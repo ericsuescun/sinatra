2e2f2fcc891bb0effc3cf56c0e9432440523f44b
Eric Suescun <edsuescun@gmail.com>
2019-02-19 16:22:36 -0500

    Added a ! symbol in the string...

--- solution.rb
+++ solution.rb
@@ -4,7 +4,7 @@ get '/' do
 	if !params[:nombre] || params[:nombre].empty?	#Unless I pass something through the URL, it will show Hello World
   		"¡Hola desconocido!"
   	else
-  		"<h1>Hola #{params[:nombre]}</h1>"	#But if there is something in the key nombre, then it will display that text
+  		"<h1>Hola #{params[:nombre]}!</h1>"	#But if there is something in the key nombre, then it will display that text
   	end
 
 end

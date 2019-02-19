5bc7aefb5ec532064bd9cc9263db53dda72da658
Eric Suescun <edsuescun@gmail.com>
2019-02-19 16:12:05 -0500

    Added a fix for non parameters
    It should work with no parameters AND parameter empty

--- solution.rb
+++ solution.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 
 get '/' do
-	if params[:nombre].empty?	#Unless I pass something through the URL, it will show Hello World
+	if !params[:nombre]	#Unless I pass something through the URL, it will show Hello World
+  		"¡Hola desconocido!"
+  	elsif params[:nombre].empty?
   		"¡Hola desconocido!"
   	else
   		"<h1>Hola #{params[:nombre]}</h1>"	#But if there is something in the key nombre, then it will display that text

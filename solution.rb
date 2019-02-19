9d3fad1a1285f3a8f74d8b58ced3d73ac4ba12d2
Eric Suescun <edsuescun@gmail.com>
2019-02-19 16:49:00 -0500

    Upload for solution.rb for the second challenge

--- solution.rb
+++ solution.rb
@@ -1,10 +1,7 @@
 require 'sinatra'
 
-get '/' do
-	if !params[:nombre] || params[:nombre].empty?	#Unless I pass something through the URL, it will show Hello World
-  		"¡Hola desconocido!"
-  	else
-  		"<h1>Hola #{params[:nombre]}!</h1>"	#But if there is something in the key nombre, then it will display that text
+get '/makers/:nombre' do
+  		"<h1>Hola #{params[:nombre].capitalize}!</h1>"	#But if there is something in the key nombre, then it will display that text
   	end
 
 end

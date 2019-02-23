9f7417df7f3406403dee29e63577e6eb549c8a19
Eric Suescun <edsuescun@gmail.com>
2019-02-23 18:29:29 -0500

    Complete methods for root and for makers without params

--- solution.rb
+++ solution.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 
 get '/makers/:nombre' do
   		"<h1>Hola #{params[:nombre].capitalize}!</h1>"	#But if there is something in the key nombre, then it will display that text
-  	end
+end
 
+get '/makers/' do
+	"¡Hola desconocido!"
+end
+
+get '/' do
+	"¡Hola desconocido!"
 end
